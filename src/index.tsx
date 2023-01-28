106cb6139e117c854de0c04b4a9df702c741b71a
Bekzod <bkzd18kholdarov@gmail.com>
2023-01-28 16:53:06 +0500

    add BrowserRoute

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+// import BrowserRouter from 'react-router-dom/BrowserRouter';
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
 );
+
+
 
